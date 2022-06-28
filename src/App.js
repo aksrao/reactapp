68d2ee02d97e4d0d4c00c4efa7241473644e9b51
akshay rao <akshay.rao@classmethod.jp>
2022-06-28 13:27:45 +0530

    commit from vscode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>this is hosted on s3</h1>
+      <h1>this is hosted on s3 through github actions</h1>
     </div>
   );
 }
