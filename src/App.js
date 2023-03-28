ab6426d982284ffae1fc115f4d42e724b5c9e509
ts-akshay.rao <ts-akshay.rao@rakuten.com>
2023-03-28 14:14:50 +0900

    basic react-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState('...loading')
+
+  useEffect(() => {
+    async function fetchData () {
+      try {
+        let data = await (await fetch('/api')).json()
+        setMessage(data.message)
+      } catch (err) {
+        setMessage(err.message)
+      }
+    }
+    fetchData()
+  })
+
   return (
     <div className="App">
-      <h1>this is hosted on s3 through github actions hugo</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
+        <p>Change me!</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
